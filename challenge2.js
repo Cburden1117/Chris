04c6978818923989809b4892af7374c26be79ce3
Christopher Burden <cburden1117@verizon.net>
2021-09-29 19:28:15 -0400

    Cant figure this out

--- challenge2.js
+++ challenge2.js
@@ -13,3 +13,45 @@
 // Hint: Use an if/else statement 
 
 // GOOD LUCK!
+
+
+let markMass;
+let markHeight;
+let johnMass;
+let johnHeight;
+let markBMI;
+let johnBMI;
+const markHigherBMI = "Is Marks BMI is more than Johns? "
+
+
+markMass = 78;
+markHeight = 1.69;
+johnMass = 92;
+johnHeight = 1.95;
+
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+
+if(markBMI) {
+    markBMI < johnBMI
+    console.log(`Marks BMI of ${markBMI} is less then Johns BMI of ${johnBMI}`)
+}    else{
+    console.log(`Marks BMI of ${markBMI} is more then Johns BMI of ${johnBMI}`)    
+};
+
+markMass = 95;
+markHeight = 1.88;
+johnMass = 85;
+johnHeight = 1.76;
+bmi = markBMI > johnBMI;
+
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+bmi = markBMI > johnBMI;
+
+if(markBMI) {
+    markBMI < johnBMI
+    console.log(`Marks BMI of ${markBMI} is less then Johns BMI of ${johnBMI}`)
+}    else{
+    console.log(`Marks BMI of ${markBMI} is more then Johns BMI of ${johnBMI}`)    
+};
