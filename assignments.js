927e4f9ad3aa0f20e66a046d5e842284adb46446
Christopher Burden <cburden1117@verizon.net>
2021-10-03 08:03:49 -0400

    New

--- assignments.js
+++ assignments.js
@@ -31,8 +31,52 @@
 //     console.log('United States of Americas population is below average');
 // }
 
-console.log('9' - '5'); // 4
-console.log('19' - '13' + '17'); // 617
-console.log('19' - '13' + 17); // 23
-console.log('123' < 57); // false
-console.log(5 + 6 + '4' + 9 - 4 - 2); // 117
+// console.log('9' - '5'); // 4
+// console.log('19' - '13' + '17'); // 617
+// console.log('19' - '13' + 17); // 23
+// console.log('123' < 57); // false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // 117
+
+// const age = 18
+// if(age === 18)console.log('You just became an adult (strict)');
+// if(age == 18)console.log('You just became an adult (loose)');
+// '18' == 18 //true   (loose)
+// '18' === 18 // false   (strict)
+
+// const favorite = Number(prompt("What's your favorite number?"));
+// console.log(favorite)
+// console.log(typeof favorite)
+
+// if(favorite === 23){ // "23" == 23 -> true
+//     console.log("You smell like a poop")
+// } else if(favorite === 7){
+//     console.log('7 is also a poop')
+// } else if(favorite === 9){
+//     console.log("9 means you're dumb")
+// } else{
+//     console.log("Number is not 23, 7 or 9")
+// }
+
+// if(favorite !== 23) console.log("Why didn't you pick a cool number?");
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if(hasDriversLicense && hasGoodVision){
+//     console.log('Sarah is able to drive!')
+// } else{
+//     console.log('Someone else should drive...')
+// }
+
+const isTired = false // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired){
+    console.log('Sarah is able to drive!')
+} else{
+    console.log('Someone else should drive...')
+}
