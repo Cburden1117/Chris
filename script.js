7e020f08bcccdab93323b4796d38fb4ab5d9bcb9
Christopher Burden <cburden1117@verizon.net>
2021-10-07 19:00:07 -0400

    fixed

--- script.js
+++ script.js
@@ -129,40 +129,40 @@ console.log(typeof null);
 // n = n - 1; // 11 - 1 = 10
 // console.log(n);
 
-// const day = 'thursday';
-// switch(day) {
-//     case 'monday': // day === 'monday'
-//         console.log('Plan course structure');
-//         console.log('Go to coding meetup');
-//         break;
-//     case 'tuesday':
-//         console.log('Prepare theory videos');
-//         break;
-//     case 'wednesday':
-//     case 'thursday':
-//         console.log('Write code examples');
-//         break;
-//     case 'friday':
-//         console.log('Record videos')
-//         break;
-//     case 'saturday':
-//     case 'sunday':
-//         console.log('Enjoy the weekend!');
-//         break;   
-//     default:
-//         console.log('Not a valid date!');  
-// }
+const day = 'fridayf';
+switch(day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos')
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend!');
+        break;   
+    default:
+        console.log('Not a valid day!');  
+}
 
-let day = 'tuesday'
-if(day = 'monday'){
-    console.log('Plan course structure')
-} else if(day = 'tuesday'){
+if(day === 'monday'){
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if(day === 'tuesday'){
     console.log('Prepare theory videos')   
-} else if(day = 'wednesday' || 'thursday'){
+} else if(day === 'wednesday' || day === 'thursday'){
     console.log('Write code examples')
-} else if (day = 'friday'){
+} else if (day === 'friday'){
     console.log('Record videos')
-} else if(day = 'saturday' || 'sunday'){
+} else if(day === 'saturday' || day === 'sunday'){
     console.log('Enjoy the weekend')
 } else if(day != 'monday' || 'tuesday' || 'wednesday' || 'thursday' || 'friday' || 'saturday' || 'sunday'){
     console.log('Not a valid day!')
